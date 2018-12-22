6c13955632a270b0fa929c37084e4b261105ef84
dickychandra21 <45659402+dickychandra21@users.noreply.github.com>
2018-12-22 20:20:48 +0700

    Add files via upload

---
+++ beku atau tidak.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a;
+	cout<<"masukkan derajat suhu = ";
+	cin>>a;
+	if (a<0)
+	cout<<"suhu" <<a<< " derajat adalah zat beku";
+	else if (a>0)
+	cout<<"suhu" <<a<< " derajat adalah zat tidak beku";
+	return 0;
+	
+}
+
